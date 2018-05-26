fe39c8eaf2dee01a6ee61339ae4b3482dca5f915
Jaap Kanbier <jaapkanbier@hotmail.com>
2018-05-26 12:06:57 +0200

    splash UI + minor JS functionality

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ config('app.name', 'SpotLess') }}</title>
+    <meta name="description" content="Een web applicatie die detecteerd of de prullenbakken van Katwijk vol zijn.">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Poppins|Roboto" rel="stylesheet">
+</head>
+<body>
+  <div class="wrapper">
+    <nav>
+      <div class="navbar" id="navbar">
+       <a href="/home">Overzicht</a>
+       <a href="/buckets">Lijst</a>
+       @if (Auth::user()->isAdmin)
+         <a href="/settings">Instellingen</a>
+       @endif
+       <a href="{{ route('logout') }}"
+          onclick="event.preventDefault();
+                        document.getElementById('logout-form').submit();">
+           Uitloggen
+       </a>
+
+       <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+           @csrf
+       </form>
+       <a href="javascript:void(0);" class="icon" onclick="dropNav()">&#9776;</a>
+      </div>
+    </nav>
+    <script>
+      function dropNav() {
+          var x = document.getElementById("navbar");
+          if (x.className === "navbar") {
+              x.className += " responsive";
+          } else {
+              x.className = "navbar";
+          }
+      }
+    </script>
+
+    <main id="main">
+        @include('messages')
+        @yield('content')
+    </main>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+  </div>
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,53 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+  <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+      <title>Eloquent</title>
+      <meta name="description" content="IPMEDT4 Project: interactie tussen Lyme patienten en zorgverleners.">
+      <meta name="author" content="Eloquent">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+      <!-- Styles -->
+      <link rel="stylesheet" href="/css/landing.css">
+  </head>
+  <body>
+    <div class="background-image">
+      {{-- Old background image --}}
+      {{-- <img src="/img/splash-bg.jpg" alt="background of tall grass"> --}}
+      <video src="/vid/splash-bg.mp4" autoplay loop muted>
+        Your browser does not support this video.
+      </video>
+    </div>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+    <div class="wrapper">
+      <main id="splash">
+        <div class="splash-left">
+          <h1>Welcome to Lyme4un't</h1>
+          <p>Krijg in 10 minuten het antwoord op uren zoeken van onzekerheid.</p>
+          <p>Nu met directe PDF service!</p>
         </div>
-    </body>
+        <div class="splash-right">
+          <button id="startBtn" type="button" name="button">Start de test</button>
+        </div>
+      </main>
+    </div>
+  </body>
+
+  <script type="text/javascript">
+    window.onload = function () {
+      setTimeout(function () {
+        document.getElementById('splash').classList.add('slide-in-top');
+      }, 300);
+
+      document.getElementById('startBtn').addEventListener("click", startTest);
+
+      function startTest() {
+        document.getElementById('splash').classList.add('slide-out-top');
+      }
+
+    }
+  </script>
+
 </html>
